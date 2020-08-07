9183916aee68b189e222dd9b7f03f3e71e6bf652
Val Constable <constableval@gmail.com>
2020-08-07 16:24:11 -0400

    2 Push

--- ProjectZoopla/src/main/java/com/page/object/model/PropertySelectionPageFactory.java
+++ ProjectZoopla/src/main/java/com/page/object/model/PropertySelectionPageFactory.java
@@ -8,38 +8,37 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class PropertySelectionPageFactory {
-	
+
 	public PropertySelectionPageFactory(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(xpath = "//*[@id='search-input-location']")
 	private WebElement Location;
-	
+
 	@FindBy(xpath = "//*[@class='button button--primary']")
 	private WebElement searchBtn;
-	
+
 	@FindBy(xpath = "//*[@class='listing-results-price text-price']")
 	private List<WebElement> listprice;
-	
-	//@FindBy(xpath = "//img[@alt='Marketed by Parkmove']")
-	@FindBy(xpath="//img[@class='js-lazy-loaded']")
+
+	// @FindBy(xpath = "//img[@alt='Marketed by Parkmove']")
+	@FindBy(xpath = "//img[@class='js-lazy-loaded']")
 	private WebElement LogoImage;
-	
-	//@FindBy(xpath = "//*[@class='account-link__text']")
-	@FindBy(xpath="(//*[text()='My Zoopla'])[1]")
+
+	// @FindBy(xpath = "//*[@class='account-link__text']")
+	@FindBy(xpath = "(//*[text()='My Zoopla'])[1]")
 	private WebElement zooplalogout;
-	
-	//@FindBy(xpath = "//*[@class='icon--signout']")
-	@FindBy(xpath="(//div[@content='Sign out'])[1]")
+
+	// @FindBy(xpath = "//*[@class='icon--signout']")
+	@FindBy(xpath = "(//div[@content='Sign out'])[1]")
 	private WebElement zooplaSignout;
-	
-	
+
 	public WebElement getLocation() {
-		return Location ;
+		return Location;
 	}
 
-    public WebElement getSearchBtn() {
+	public WebElement getSearchBtn() {
 		return searchBtn;
 	}
 
